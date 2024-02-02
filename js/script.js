f320e25084de3b8e99e0b8f71ff8de5a2551ff1a
HenriquePNZ <h254260@dac.unicamp.br>
2024-02-02 01:51:28 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+let interval; // Variável para armazenar o intervalo de tempo
+
+function hideSlide() {
+    slider.forEach(item => item.classList.remove('on'));
+}
+
+function showSlider() {
+    slider[currentSlide].classList.add('on');
+}
+
+function nextSlider() {
+    hideSlide();
+
+    if (currentSlide == slider.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+
+    showSlider();
+}
+
+function prevSlider() {
+    hideSlide();
+
+    if (currentSlide == 0) {
+        currentSlide = slider.length - 1;
+    } else {
+        currentSlide--;
+    }
+
+    showSlider();
+}
+
+// Adiciona um intervalo para chamar a função nextSlider() a cada 5 segundos
+function startAutoSlide() {
+    interval = setInterval(nextSlider, 5000);
+}
+
+// Inicia o slideshow automaticamente ao carregar a página
+startAutoSlide();
+
+// Adiciona listeners para os botões de navegação
+btnNext.addEventListener('click', nextSlider);
+btnPrev.addEventListener('click', prevSlider);
+
+// Função para redirecionar para outras páginas
+function redirecionarParaPagina(event) {
+    clearInterval(interval); // Limpa o intervalo atual
+    startAutoSlide(); // Reinicia o slideshow automático
+
+    if (event.target.classList.contains('quemSomos')) {
+        window.location.href = "quemSomos.html";
+    } else if (event.target.classList.contains('orientacoes')) {
+        window.location.href = "orientacoes.html";
+    } else if (event.target.classList.contains('calendario')) {
+        window.location.href = "calendario.html";
+    } else if (event.target.classList.contains('cursos')) {
+        window.location.href = "cursos.html";
+    }
+}
